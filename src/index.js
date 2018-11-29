4f8754a98e4824b60621c7e2a59dc58e9246f8df
Sean Doherty <sean.doherty@crosscomm.net>
2018-11-28 22:43:50 -0500

    Changing initial import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './index.css';
 
 // For the problem of sporadic state example
-// import './sporadic-state-problem'
+import './sporadic-state-problem'
 
 // For the problem of sporadic state answer
 // import './sporadic-state-solution'
 
 // For the centralized state example
-import './centralized-state'
+// import './centralized-state'
