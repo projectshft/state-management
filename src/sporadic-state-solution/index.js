86b19fe45c9db62e3a55fc9fbb5575bf9202c5af
Tucker Blackwell <36317675+trblackw@users.noreply.github.com>
2018-11-29 09:34:59 -0500

    Modified used of context
    Removed defaultValue param passed to React.createContext; destructured { Component } from React for consistency throughout previous lessons; added some notes regarding the object passed to value prop

--- src/sporadic-state-solution/index.js
+++ src/sporadic-state-solution/index.js
@@ -1,47 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 // Setup the context object for our user information
-const UserContext = React.createContext('user');
+const UserContext = React.createContext();
 
-class Hello extends React.Component {
-  render () {
+//grab the Consumer & Provider off of the context instance
+//each of these will have a context property that will contain the state we need
+//alternatively, we could user <UserContext.Consumer /> or <UserContext.Provider />
+const Consumer = UserContext.Consumer;
+const Provider = UserContext.Provider;
+
+class Hello extends Component {
+  render() {
     return (
-      <h1>Hello, <IsItMe/>!</h1>
+      <h1>
+        Hello, <IsItMe />!
+      </h1>
     );
   }
 }
 
-class IsItMe extends React.Component {
-  render () {
-    return (
-      <YoureLookingFor/>
-    );
+class IsItMe extends Component {
+  render() {
+    return <YoureLookingFor />;
   }
 }
 
-class YoureLookingFor extends React.Component {
-  render () {
+class YoureLookingFor extends Component {
+  render() {
     return (
-    // Create a consumer of the context that will subscribe to changes for the value
-    <UserContext.Consumer>
-      {({isItLionel}) => {
-        if (isItLionel === true) {
-          return (
-            <span>Lionel</span>
-          );
-        } else {
-          return (
-            <span>You</span>
-          )
-        }
-      }}
-    </UserContext.Consumer>
-  )}3
+      // Create a consumer of the context that will subscribe to changes for the value
+      <Consumer>
+        {({ isItLionel }) => {
+          if (isItLionel === true) {
+            return <span>Lionel</span>;
+          } else {
+            return <span>You</span>;
+          }
+        }}
+      </Consumer>
+    );
+  }
+  3;
 }
 
 // This is a simpler implementation where you can programmatically access the context object and do what you want with it.
-// class YoureLookingFor extends React.Component {
+// class YoureLookingFor extends Component {
 //   static contextType = UserContext;
 //   render () {
 //     if (this.context.isItLionel === true) {
@@ -57,12 +61,16 @@ class YoureLookingFor extends React.Component {
 // }
 
 // Cache the element we'll be rendering to.
-const root = document.querySelector('#root');
+const root = document.querySelector("#root");
 
 // Setup the provider with the default values
+//Note that we're passing an object as the value prop to the Provider, rather than just a boolean or single value. In doing this,
+//we're able to pass multiple properties to the single value prop:
+//i.e. value={{ isItLionel: true, isHandsome: false, instruments: ['piano', 'vocals', 'saxophone'] }}
+//we would then have access to all of these properties via context
 ReactDOM.render(
-  <UserContext.Provider value={{isItLionel:true}}>
-    <Hello/>
-  </UserContext.Provider>,
+  <Provider value={{ isItLionel: true }}>
+    <Hello />
+  </Provider>,
   root
 );
